f64f5058d8c7746b5c76bd98f3db8b15b9fa3382
Ubuntu <ubuntu@fv4ak3294dnt8fabu0bs.auto.internal>
2026-02-14 21:33:38 +0000

    Правки settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,8 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
-
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.210.182', 'kittyfree.hopto.org', 'localhost', '127.0.0.1']
